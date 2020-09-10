995070585e9e0f16bebe66c32d6ab2ebff977ee0
WalnutSheep <69225633+WalnutSheep@users.noreply.github.com>
2020-09-10 11:49:07 +1000

    functions aquarium

--- aquarium.js
+++ aquarium.js
@@ -4,22 +4,70 @@ window.onload=function() {
 	heightObj = document.getElementById('txtHeight');
     costObj = document.getElementById('tdCost');
     document.getElementById('btnReset').onclick = resetInputs;
-    document.getElementById('btnCalcCost').onclick = calcCost;
+    document.getElementById('btncalcCost').onclick = calcCost;
 }
+
 
 function resetInputs() {
     lengthObj.value = '';
-    girthObj.value = '';
-    weightObj.innerHTML = '';
+    widthObj.value = '';
+	heightObj.value = '';
+    costObj.innerHTML = '';
 }
+
+function glue () {
+	var length = new number (lengthObj.value);
+	var width = new number (widthObj.value);
+	var height = new number (heightObj.value);
+	var cost = 0.1;
+	return ((length + width * 2) + (height*4) * cost);
+}
+
+
+
+function glass () {
+	var length = new Number ( lengthObj.value);
+	var width = new Number (widthObj.value);
+	var height = new Number ( heightObj.value);
+	var cost = 0.1;
+	return ((length * width) + (height* length *2 )+ (height * width *2)) * cost;
+}
+
+
+function labour () {
+	var length = new Number ( lengthObj.value);
+	var width = new Number (widthObj.value);
+	var height = new Number ( heightObj.value);
+	var surfacearea = glass ();
+	var labourcost = 60;
+	var labourMath = labourCost/6000;
+	return surfaceArea * labourMath;
+}	
+
+
+function gst () {
+	var length = new Number (lengthObj.value);
+	var width = new Number (widthObj.value);
+	var height= new Number (heightObj.value);
+	return ((labour + glue + glass) *0.01) ;
+}
+
 
 function calcCost() {
     var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
-    costObj.innerHTML = '';
+	var height = new Number(widthObj.value);
+	var gluecost = glue();
+	var labourCost = labour();
+	var glassCost = glass();
+	var tax = gst();
+	
     if(isNaN(length) || isNaN(width)) {
         alert('Invalid length or width');
         return;
     }
-    costObj.innerHTML = length*Math.pow(width,2)/800;
+	
+
+    costObj.innerHTML = '';
+	costObj.innerHTML = gluecost + labour Cost+ glassCost + tax;
 }
